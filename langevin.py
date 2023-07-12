e7fcaa8d396db5c03931e695769ad479f0b7b308
M-Ismerio <129776710+M-Ismerio@users.noreply.github.com>
2023-07-12 13:58:49 -0300

    Update langevin.py
    Change fontsize and style

--- langevin.py
+++ langevin.py
@@ -62,10 +62,10 @@ def solve_langevin(parameters, tmax=400, PLOT=True):
     plt.rcParams['mathtext.fontset'] = 'stix'
     fig, ax = plt.subplots(figsize=(12, 3))
     ax.plot(save_times, save_T)
-    ax.set_xticks(arange(0, pi/w*17, pi/w), arange(0, 17, 1), fontsize=13)
-    ax.set_xlabel(r'$t$ [$10^5$ years]', fontsize=13)
-    ax.set_yticks(arange(-2*T_2, 3*T_2, T_2), [r'$-2T_0$', r'$-T_0$', '0', r'$T_0$', r'$2T_0$'], fontsize=13)
-    ax.set_ylabel(r'$T$', fontsize=13)
+    ax.set_xticks(arange(0, pi/w*17, pi/w), arange(0, 17, 1), fontsize=16)
+    ax.set_xlabel(r'$t$ [$10^5$ years]', fontsize=16)
+    ax.set_yticks(arange(-2*T_2, 3*T_2, T_2), [r'$-2T_0$', r'$-T_0$', '0', r'$T_0$', r'$2T_0$'], fontsize=16)
+    ax.set_ylabel(r'$T$', fontsize=16)
     plt.tight_layout()
     plt.savefig('EstMec_1_StochasticResonance.png', dpi=600)
     plt.show()
@@ -92,7 +92,7 @@ ax[0].text(0.5, 0.9, r'$t = 0$', transform=ax[0].transAxes, fontsize=13)
 ax[1].text(0.5, 0.9, r'$t = \pi/2\omega$', transform=ax[1].transAxes, fontsize=13)
 ax[2].text(0.5, 0.9, r'$t = \pi/\omega$', transform=ax[2].transAxes, fontsize=13)
 plt.tight_layout()
-#plt.savefig('EstMec_0_Potential.png', dpi=600)
+plt.savefig('EstMec_0_Potential.png', dpi=600)
 plt.show()
 
 ###########################################################################################
@@ -110,7 +110,7 @@ ax.set_xticks(arange(-15, 20, 5), [r'$-3T_0/2$', r'$-T_0$', r'$-T_0/2$', r'$0$',
 ax.set_xlabel(r'$T$', fontsize=13)
 ax.set_ylabel(r'Probability Distribution', fontsize=13)
 plt.tight_layout()
-#plt.savefig('EstMec_2_PDF.png', dpi=600)
+plt.savefig('EstMec_2_PDF.png', dpi=600)
 plt.show()
 
 ###########################################################################################
@@ -121,26 +121,26 @@ T_2 = 10
 ax1 = ax[0]
 x, y = solve_langevin([100, -10, 10, 10, 0.25/2, 1], PLOT=False)
 ax1.plot(x, y)
-ax1.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
-ax1.set_ylabel(r'Low $g$', fontsize=13)
+ax1.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
+ax1.set_ylabel(r'Low $g$', fontsize=16)
 
 # High noise, high force strength 
 ax2 = ax[1]
 x, y = solve_langevin([100, -10, 10, 10, 0.25/2, 7], PLOT=False)
 ax2.plot(x, y)
-ax2.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
+ax2.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
 
 # Low force strength
 ax3 = ax[2]
 x, y = solve_langevin([100, -10, 10, 1, 0.25/2, 7], PLOT=False)
 ax3.plot(x, y)
-ax3.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
+ax3.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
 w = 0.25/2
-ax3.set_xticks(arange(0, pi/w*17, pi/w), arange(0, 17, 1), fontsize=13)
-ax3.set_xlabel(r'$t$ [$10^5$ years]', fontsize=13)
-ax3.set_ylabel(r'Low $\varepsilon$', fontsize=13)
+ax3.set_xticks(arange(0, pi/w*17, pi/w), arange(0, 17, 1), fontsize=16)
+ax3.set_xlabel(r'$t$ [$10^5$ years]', fontsize=16)
+ax3.set_ylabel(r'Low $\varepsilon$', fontsize=16)
 
-#plt.savefig('EstMec_3_ComparisonAmplitude.png', dpi=600)
+plt.savefig('EstMec_3_ComparisonAmplitude.png', dpi=600)
 plt.show()
 
 ###########################################################################################
@@ -149,11 +149,11 @@ fig, ax = plt.subplots(3, 1, figsize = (10,6), sharex=True)
 T_2 = 10
 # High Frequency
 ax1 = ax[0]
-w = 0.5
+w = 1
 x, y = solve_langevin([100, -10, 10, 17, w, 7], PLOT=False)
 ax1.plot(x, y)
-ax1.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
-ax1.set_ylabel(r'High $\omega$', fontsize=13)
+ax1.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
+ax1.set_ylabel(r'High $\omega$', fontsize=16)
 
 # Kramer Frequency
 ax2 = ax[1]
@@ -162,17 +162,17 @@ g = 7
 w_k = sqrt(2)*U_0/10*exp(-2*U_0/g/g)/2
 x, y = solve_langevin([100, -10, 10, 17, w_k, 7], PLOT=False)
 ax2.plot(x, y)
-ax2.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
+ax2.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
 
 # Low frequency
 ax3 = ax[2]
-w = 0.05/2
+w = 0.05/5
 x, y = solve_langevin([100, -10, 10, 17, w, 7], PLOT=False)
 ax3.plot(x, y)
-ax3.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=13)
-ax3.set_xticks(arange(0, pi/w_k*16, pi/w_k), arange(0, 16, 1), fontsize=13)
-ax3.set_xlabel(r'$t$ [$10^5$ years]', fontsize=13)
-ax3.set_ylabel(r'Low $\omega$', fontsize=13)
+ax3.set_yticks(arange(-T_2, 2*T_2, T_2), [r'$-T_0$', '0', r'$T_0$'], fontsize=16)
+ax3.set_xticks(arange(0, pi/w_k*16, pi/w_k), arange(0, 16, 1), fontsize=16)
+ax3.set_xlabel(r'$t$ [$10^5$ years]', fontsize=16)
+ax3.set_ylabel(r'Low $\omega$', fontsize=16)
 
-#plt.savefig('EstMec_4_ComparisonOmega.png', dpi=600)
+plt.savefig('EstMec_4_ComparisonOmega.png', dpi=600)
 plt.show()
